b484105c66e5564970a69516e0d0216bdb0c7221
Sebastian Rys <sebastian.rys.95@gmail.com>
2019-04-01 23:21:50 +0200

    Added new views

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,4 +1,4 @@
-""""URL adresses for learning_logs """
+""""URL addresses for learning_logs """
 
 from django.conf.urls import url
 from . import views
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^topics/$', views.topics, name='topics'),  # topics page
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),  # detailed page of topic
     url(r'^new_topic/$', views.new_topic, name='new_topic'),    # create topic by user page
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),    # Create entry by user page
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
