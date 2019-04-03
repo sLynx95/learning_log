44cb44f53bcfe1a5ffe1cd486ca269b821c8e3a2
Sebastian Rys <sebastian.rys.95@gmail.com>
2019-04-03 23:55:36 +0200

    Added log out from app

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import url
 from django.urls import path, include
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView
 
-# from . import views
+from . import views
 
 urlpatterns = [
-   url(r'^login/$', views.LoginView.as_view(), {'template_name': 'users/login.html'}, name='login'),   # login page
+   url(r'^login/$', LoginView.as_view(), {'template_name': 'users/login.html'}, name='login'),   # login page
+   url(r'^logout/$', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:topics'))
+
